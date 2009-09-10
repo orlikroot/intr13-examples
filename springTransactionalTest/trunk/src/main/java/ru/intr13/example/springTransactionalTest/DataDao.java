df3911289a468de2f1afab2386d5dd80e65c8d50
kiselev <kiselev@c6fc7d16-9cfe-11de-a36b-d11e950a0b04>
2009-09-10 13:25:41 +0000

    Share project "springTransactionalTest" into "https://intr13-examples.googlecode.com/svn"

---
+++ springTransactionalTest/trunk/src/main/java/ru/intr13/example/springTransactionalTest/DataDao.java
@@ -0,0 +1,13 @@
+package ru.intr13.example.springTransactionalTest;
+
+import java.util.Collection;
+
+public interface DataDao {
+
+	Data get(Long id);
+
+	Data save(Data data);
+
+	Collection<Data> find(String text);
+
+}
