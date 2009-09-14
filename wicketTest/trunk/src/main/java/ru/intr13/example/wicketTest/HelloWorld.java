8d4e74691d93e4c82e00b1bfb4b300830c7da402
kiselev <kiselev@c6fc7d16-9cfe-11de-a36b-d11e950a0b04>
2009-09-14 14:35:50 +0000

    add layout

--- wicketTest/trunk/src/main/java/ru/intr13/example/wicketTest/HelloWorld.java
+++ wicketTest/trunk/src/main/java/ru/intr13/example/wicketTest/HelloWorld.java
@@ -6,5 +6,6 @@ import org.apache.wicket.markup.html.basic.Label;
 public class HelloWorld extends WebPage {
 	public HelloWorld() {
 		add(new Label("message", "kva"));
+		add(new HelloWorldBorder("helloWorldBorder"));
 	}
 }
---
+++ wicketTest/trunk/src/main/java/ru/intr13/example/wicketTest/HelloWorldBorder.java
@@ -0,0 +1,13 @@
+package ru.intr13.example.wicketTest;
+
+import org.apache.wicket.markup.html.border.Border;
+
+public class HelloWorldBorder extends Border {
+
+	public HelloWorldBorder(String id) {
+		super(id);
+	}
+
+	private static final long serialVersionUID = -7140852948252493352L;
+
+}
