0a99411bf51f7e5939710da21f71b37cd32b2d8b
kiselev <kiselev@c6fc7d16-9cfe-11de-a36b-d11e950a0b04>
2010-05-26 14:22:09 +0000

    TransactionSynchronizationRegistry is working!

--- TransactionalCache/ejb3-api/src/main/java/ru/intr13/example/jboss/transactionalCache/ITestService.java
+++ TransactionalCache/ejb3-api/src/main/java/ru/intr13/example/jboss/transactionalCache/ITestService.java
@@ -1,5 +1,5 @@
 package ru.intr13.example.jboss.transactionalCache;
 
 public interface ITestService {
-	Long sum(Long first, Long second);
+	Long sum(Long first, Long second) throws Exception;
 }
--- TransactionalCache/ejb3/src/main/java/ru/intr13/example/jboss/transactionalCache/TestService.java
+++ TransactionalCache/ejb3/src/main/java/ru/intr13/example/jboss/transactionalCache/TestService.java
@@ -1,15 +1,36 @@
 package ru.intr13.example.jboss.transactionalCache;
 
+import javax.annotation.Resource;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.transaction.TransactionSynchronizationRegistry;
 
 @Remote(ITestService.class)
-@Stateless(name="TestService")
+@Stateless(name = "TestService")
 public class TestService implements ITestService {
 
+	@Resource(mappedName = "java:comp/TransactionSynchronizationRegistry")
+	TransactionSynchronizationRegistry transactionSynchronizationRegistry;
+
 	@Override
-	public Long sum(Long first, Long second) {
-		return first + second;
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	public Long sum(Long first, Long second) throws Exception {
+		Long result = (Long) transactionSynchronizationRegistry
+				.getResource("kva");
+		System.out.println("Old: " + result);
+
+		result = (result == null ? 0 : result) + first;
+		transactionSynchronizationRegistry.putResource("kva", result);
+
+		result = (Long) transactionSynchronizationRegistry.getResource("kva");
+		System.out.println("Old: " + result);
+
+		result = (result == null ? 0 : result) + second;
+		transactionSynchronizationRegistry.putResource("kva", result);
+
+		return result;
 	}
 
 }
